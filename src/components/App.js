22e6ab1910be2cfd1138e8f2d59f80c4c58d169a
Fabian Almenara <tufac2@gmail.com>
2019-08-17 21:39:16 +0200

    Header

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import PostList from './PostList';
 import PostDetail from './PostDetail';
+import Header from './header/Header.jsx';
 
 const App = () => {
   return(
     <div className="ui container grid">
+      <Header />
       <div className="ui row">
         <div className="column eight wide">
           <PostList />
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => (
+  <div className="ui three item menu">
+    <a className="item">Editorials</a>
+    <a className="item">Reviews</a>
+    <a className="item active">Upcoming Events</a>
+  </div>
+)
+
+export default Header;
