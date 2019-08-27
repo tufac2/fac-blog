c583a41da880763915e5ad95b3996a594fb83dd4
Fabián Almenara <fabian.almenara@elca.ch>
2019-08-27 14:49:22 +0200

    Side header first approach

--- src/components/SideHeader/SideHeader.jsx
+++ src/components/SideHeader/SideHeader.jsx
@@ -1,20 +1,69 @@
 import React from 'react'
 
-const SideHeader = () => (
-  <div id="site_header" className="ui three item menu sideHeader">
-    <div className="my-photo">
-      <img src="images/my_photo.jpg" alt="image" />
-      <div className="mask"></div>
-    </div>
-    <div className="site-title-block">
-      <h1 className="site-title">Fabián Almenara</h1>
-      <p className="site-description">Organizational change for a better project management</p>
-      <a href="https://lmpixels.com/demo/sunshine-demo/sunshine-version-1/sunshine-v1-html-template/index.html#blog"></a>
-    </div>
-    <a className="menu-toggle mobile-visible">
-      <i className="fa fa-bars"></i>
-    </a>
-  </div>
-)
+import {
+  Button,
+  Header,
+  Icon,
+  Image,
+  Menu,
+  Segment,
+  Sidebar,
+} from 'semantic-ui-react'
 
-export default SideHeader;
+export default class SideHeader extends React.Component {
+  state = { visible: false }
+
+  handleHideClick = () => this.setState({ visible: false })
+  handleShowClick = () => this.setState({ visible: true })
+  handleSidebarHide = () => this.setState({ visible: false })
+
+  render() {
+    const { visible } = this.state
+
+    return (
+      <div>
+        <Button.Group>
+          <Button disabled={visible} onClick={this.handleShowClick}>
+            Show sidebar
+          </Button>
+          <Button disabled={!visible} onClick={this.handleHideClick}>
+            Hide sidebar
+          </Button>
+        </Button.Group>
+
+        <Sidebar.Pushable as={Segment}>
+          <Sidebar
+            as={Menu}
+            animation='overlay'
+            icon='labeled'
+            inverted
+            onHide={this.handleSidebarHide}
+            vertical
+            visible={visible}
+            width='thin'
+          >
+            <Menu.Item as='a'>
+              <Icon name='home' />
+              Home
+            </Menu.Item>
+            <Menu.Item as='a'>
+              <Icon name='gamepad' />
+              Games
+            </Menu.Item>
+            <Menu.Item as='a'>
+              <Icon name='camera' />
+              Channels
+            </Menu.Item>
+          </Sidebar>
+
+          <Sidebar.Pusher>
+            <Segment basic>
+              <Header as='h3'>Application Content</Header>
+              <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' />
+            </Segment>
+          </Sidebar.Pusher>
+        </Sidebar.Pushable>
+      </div>
+    )
+  }
+}
