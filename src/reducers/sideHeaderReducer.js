c007ba6727c2954fbaeb4c79a85f2f8bf8e8da2f
Fabian Almenara <tufac2@gmail.com>
2019-08-28 19:29:16 +0200

    initializing reducer

--- src/reducers/sideHeaderReducer.js
+++ src/reducers/sideHeaderReducer.js
@@ -1,3 +1,10 @@
-export default (state, action) => {
-
+export default (state = false, action) => {
+  switch (action.type) {
+    case 'SET_SIDE_HEADER':
+      return action.payload
+      break;
+    default:
+      return status
+      break;
+  }
 }
