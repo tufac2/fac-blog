fccf2762618bcbb0719c2d70b9a6e366d5d9371b
Fabian Almenara <tufac2@gmail.com>
2019-08-30 19:56:02 +0200

    Refactor plus

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import PostList from './PostList';
-import PostDetail from './PostDetail';
 import SideHeader from './SideHeader/SideHeader.js';
 
 import { Sidebar, Segment } from 'semantic-ui-react';
@@ -13,9 +12,6 @@ const App = () => {
         <div className="ui vertical stripe quote segment">
           <div className="ui equal width stackable internally celled grid">
             <PostList />
-          </div>
-          <div className="column eight wide">
-            <PostDetail />
           </div>
         </div>
       </SideHeader>
